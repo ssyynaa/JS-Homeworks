939b95d4c607e2ca1ad742fc968c0aa1dfeb368e
Yana <sosnovskaya_00@inbox.ru>
2024-10-21 20:16:57 +0300

    started cw3

---
+++ cw3/script.js
@@ -0,0 +1,27 @@
+/*
+
+while(true) {
+    //code
+}
+
+do {
+    //code
+} while(true);
+
+for(let a = 2, ..; true;) {
+    //code
+    //break/continue;
+}
+
+break;
+continue;
+
+*/
+
+let count = 0;
+
+while(count <= 3) {
+    console.log('Привет');
+
+    count++; //count = count + 1;
+}
